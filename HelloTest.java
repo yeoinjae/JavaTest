ba39ef71f5f1d26078b6a722256a7655aedc415d
yeoinjae <ja72762419@gmail.com>
2017-03-22 16:13:49 +0900

    Version 0.2
    Version 0.2

---
+++ HelloTest.java
@@ -0,0 +1,15 @@
+package main;
+
+public class HelloTest {
+
+	String name;
+	
+	
+	HelloTest mt = new HelloTest() {};
+
+	@Override
+	public String toString() {
+		return "HelloTest [name=" + name + "]";
+	}
+
+}
